6837a1111094586cbb104d4466d59e099df88b81
Danny-MCD <G00410864@atu.ie>
2024-04-30 12:03:33 +0100

    update to assignment 4

--- Assignments/Assignment 04/assignment04-github.py
+++ Assignments/Assignment 04/assignment04-github.py
@@ -14,21 +14,27 @@ g = Github(apikey)
 repo = g.get_repo("Danny-MCD/APrivateOne")
 print(repo.clone_url)
 
-# Access content of text file and downloading URL
-FileInfo = repo.get_contents("assignment05.txt")
-URLofFile = FileInfo.download_url
-print(URLofFile)
+try:
+    # Access content of text file and downloading URL
+    FileInfo = repo.get_contents("assignment05.txt")
+    URLofFile = FileInfo.download_url
+    print(URLofFile)
 
-# Sending GET request & extracting contents
-response = requests.get(URLofFile)
-ContentofFile = response.text
-print(ContentofFile)
+    # Sending GET request & extracting contents
+    response = requests.get(URLofFile)
+    # Raise an error if request fails
+    response.raise_for_status()
+    ContentofFile = response.text
+    print(ContentofFile)
 
-# Replace names in file
-NewContent = ContentofFile.replace("Andrew", "Danny")
-print(NewContent)
+    # Replace names in file
+    NewContent = ContentofFile.replace("Andrew", "Danny")
+    print(NewContent)
 
-#Update text file and commit
-result_changed = repo.update_file(FileInfo.path, "All instances of Andrew replaced with Danny", NewContent, FileInfo.sha, branch="main")
-print(result_changed)
+    #Update text file and commit
+    repo.update_file("assignment05.txt", "Updated by script", NewContent, FileInfo.sha)
+    print("File updated successfully.")
+
+except Exception as e:
+    print("An error occurred:", e)
 
